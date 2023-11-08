8ec94e6d7fbcbe1210129991944a76d27274d0d2
Daniel <daniel.bachmann2@students.fhnw.ch>
2023-11-08 22:51:56 +0100

    transmitter finished

--- 2_vhdl/ip/transmitter.vhd
+++ 2_vhdl/ip/transmitter.vhd
@@ -14,8 +14,8 @@ entity transmitter is
 		irst_n : in  std_ulogic; -- asynchronous reset, active low
 
 		send_data_in : in std_ulogic; 
-		--send_one_faulty_bit_in : in std_ulogic;
-		--send_two_faulty_bits_in : in std_ulogic;
+		send_one_faulty_bit_in : in std_ulogic;
+		send_two_faulty_bits_in : in std_ulogic;
 
 		data_tx_in : in std_ulogic_vector(7 downto 0);
 		
@@ -27,8 +27,16 @@ end transmitter;
 
 architecture rtl of transmitter is
 	signal rst_reg    : std_ulogic_vector(1 downto 0);
-	signal cnt : integer range 8 downto 0;
-	signal stored_data : std_ulogic_vector(7 downto 0);
+	signal cnt : integer range 9 downto 0;
+	signal freq : integer range 11 downto 0;
+	signal stored_data : std_ulogic_vector(8 downto 0);
+	signal spi_clk : std_ulogic;
+	signal prev_data_in_state: std_ulogic;
+	signal data_in_edge: std_logic;
+	signal prev_one_faulty: std_ulogic;
+	signal one_faulty_edge: std_ulogic;
+	signal prev_two_faulty: std_ulogic;
+	signal two_faulty_edge: std_ulogic;
   
 	type t_state is (
 	  idle,
@@ -57,8 +65,15 @@ begin
 		if irst_n = '0' then
 			stored_data <= (others => '0'); 
 		elsif rising_edge(clk) then
-			if send_data_in = '1' then
-				stored_data <= data_tx_in;
+			if data_in_edge = '1' then
+				stored_data <= data_tx_in & xor data_tx_in;
+			elsif one_faulty_edge = '1' then
+				stored_data <= data_tx_in & xor data_tx_in;
+				stored_data(8) <= not stored_data(8);
+			elsif two_faulty_edge = '1' then
+				stored_data <= data_tx_in & xor data_tx_in;
+				stored_data(7) <= not stored_data(7);
+				stored_data(5) <= not stored_data(5);
 			end if;
 		end if;
 	end process;
@@ -66,28 +81,34 @@ begin
 	
 	p_next_state : process(all)
 	begin
-		case current_state is
-		when idle => 
-			if send_data_in = '1' then 
+		if current_state = idle then
+			if data_in_edge = '1' then
+				next_state <= set_cs_n;
+			elsif one_faulty_edge = '1' then
+				next_state <= set_cs_n;
+			elsif two_faulty_edge ='1' then
 				next_state <= set_cs_n;
 			else
 				next_state <= idle;
 			end if;
-		when set_cs_n => 
-				next_state <= sck_low;
-		when sck_low => 
-				next_state <= sck_high;
-		when sck_high => 			
-			if cnt = 0 then 
-				next_state <= delay;
-			else
-				next_state <= sck_low;
-			end if;
-		when delay =>
+		elsif spi_clk = '1' then
+			case current_state is
+			when set_cs_n => 
+					next_state <= sck_low;
+			when sck_low => 
+					next_state <= sck_high;
+			when sck_high => 			
+				if cnt = 0 then 
+					next_state <= delay;
+				else
+					next_state <= sck_low;
+				end if;
+			when delay =>
+					next_state <= idle;
+			when others =>
 				next_state <= idle;
-		when others =>
-			next_state <= idle;
-		end case;
+			end case;
+		end if;
 	end process p_next_state;
 	
 	p_output : process(all)
@@ -120,26 +141,92 @@ begin
 	p_count : process(irst_n, clk)
 	begin
 		if irst_n = '0' then
-		cnt <= 7;
+		cnt <= 8;
 		elsif rising_edge(clk) then
-			if current_state = sck_high then
-				if cnt = 0 then
-					cnt <= 7;
-				else
-					cnt <= cnt - 1;
+			if spi_clk = '1' then
+				if current_state = sck_high then
+					if cnt = 0 then
+						cnt <= 8;
+					else
+						cnt <= cnt - 1;
+					end if;
 				end if;
-			end if;
+        	end if;
 		end if;
 	end process p_count;
 	  
+
 	p_current_stat: process(clk, irst_n)
 	begin
 		if irst_n = '0' then
 			current_state <= idle;
 		elsif rising_edge(clk) then
-			current_state <= next_state;
+				current_state <= next_state;
 		end if;
 	end process p_current_stat;
-	
+
+	p_spi_clk: process(clk, irst_n)
+	begin
+		if irst_n = '0' then
+			freq <= 0;
+			spi_clk <= '0';
+		elsif rising_edge(clk) then
+			if freq = 4 then
+				spi_clk <= '1';
+				freq <= freq + 1;
+			elsif freq = 5 then
+				spi_clk <= '0';
+				freq <= 0;
+			else
+				freq <= freq + 1;
+			end if;
+		end if;
+	end process p_spi_clk;
+
+	p_data_in_edge: process(clk, irst_n)
+  	begin
+		if irst_n = '0' then
+			prev_data_in_state <= '0';
+			data_in_edge <= '0';
+    	elsif rising_edge(clk) then
+			if send_data_in = '1' and prev_data_in_state = '0' then
+				data_in_edge <= '1';
+			else
+				data_in_edge <= '0';
+			end if;
+			prev_data_in_state <= send_data_in;
+		end if;
+    end process p_data_in_edge;
+
+	p_one_faulty_edge: process(clk, irst_n)
+  	begin
+		if irst_n = '0' then
+			prev_one_faulty <= '0';
+			one_faulty_edge <= '0';
+    	elsif rising_edge(clk) then
+			if send_one_faulty_bit_in = '1' and prev_one_faulty = '0' then
+				one_faulty_edge <= '1';
+			else
+				one_faulty_edge <= '0';
+			end if;
+			prev_one_faulty <= send_one_faulty_bit_in;
+		end if;
+    end process p_one_faulty_edge;
+
+	p_two_faulty_edge: process(clk, irst_n)
+  	begin
+		if irst_n = '0' then
+			prev_two_faulty <= '0';
+			two_faulty_edge <= '0';
+    	elsif rising_edge(clk) then
+			if send_two_faulty_bits_in = '1' and prev_two_faulty = '0' then
+				two_faulty_edge <= '1';
+			else
+				two_faulty_edge <= '0';
+			end if;
+			prev_two_faulty <= send_two_faulty_bits_in;
+		end if;
+    end process p_two_faulty_edge;
+
 	
 end rtl;
