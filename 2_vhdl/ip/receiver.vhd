83d1e223f032f866bd62f79dba7f5c06e75b3794
Richard Wassmer <richard.wassmer@students.fhnw.ch>
2023-11-09 21:18:14 +0100

    receiver funktional allein getestet

--- 2_vhdl/ip/receiver.vhd
+++ 2_vhdl/ip/receiver.vhd
@@ -26,12 +26,12 @@ end receiver;
 architecture rtl of receiver is
 	type fsm_state is (IDLE, RECEIVE);
 	signal rx_buffer : std_ulogic_vector(8 downto 0);
-	signal bit_index : integer range 0 to 9; 
+	signal n_received_bits : integer range 0 to 9 := 0; 
 	signal current_state : fsm_state := IDLE;
 	
-	signal sync_spi_data_in : std_ulogic_vector(2 downto 0);
-	signal sync_spi_cs_in : std_ulogic_vector(3 downto 0);
-	signal sync_spi_clk_in : std_ulogic_vector(3 downto 0);
+	signal sync_spi_data_in : std_ulogic_vector(1 downto 0);
+	signal sync_spi_cs_in : std_ulogic_vector(2 downto 0);
+	signal sync_spi_clk_in : std_ulogic_vector(2 downto 0);
 
 begin
 	-- synchronize spi input signals to system clk
@@ -42,9 +42,9 @@ begin
 			sync_spi_cs_in <= (others => '1');
 			sync_spi_clk_in <= (others => '0');
 		elsif rising_edge(clk) then
-			sync_spi_data_in <= sync_spi_data_in(1 downto 0) & spi_data_in;
-			sync_spi_cs_in <= sync_spi_cs_in(2 downto 0) & spi_cs_in;
-			sync_spi_clk_in <= sync_spi_clk_in(2 downto 0) & spi_clk_in;
+			sync_spi_data_in <= sync_spi_data_in(0) & spi_data_in;
+			sync_spi_cs_in <= sync_spi_cs_in(1 downto 0) & spi_cs_in;
+			sync_spi_clk_in <= sync_spi_clk_in(1 downto 0) & spi_clk_in;
 		end if;
 		
 	end process p_sync_spi_inputs;
@@ -69,50 +69,54 @@ begin
 			case current_state is
 			
 				when IDLE =>
-					if sync_spi_cs_in(3) = '1' and sync_spi_cs_in(2) =  '0' then  -- falling edge sync_spi_cs_in
+					if sync_spi_cs_in(2) = '1' and sync_spi_cs_in(1) =  '0' then  -- falling edge sync_spi_cs_in
 						next_state := RECEIVE;
 						data_rx_out <= (others => '0');
 					else
 						next_state := current_state;
 					end if;
 					
-					bit_index <= 0;
 					rx_buffer <= (others => '0');
+					n_received_bits <= 0;
 
 					data_valid_out <= '1';
 					data_received_out <= '0';
 
 
 				when RECEIVE =>
-					if sync_spi_cs_in(3) = '0' and sync_spi_cs_in(2) = '1' then  -- rising edge sync_spi_cs_in
+					if sync_spi_cs_in(2) = '0' and sync_spi_cs_in(1) = '1' then  -- rising edge sync_spi_cs_in
 						next_state := IDLE;
 					else
 						next_state := current_state;
 					end if;
 					
-					if sync_spi_clk_in(3) = '0' and sync_spi_clk_in(2) = '1' and bit_index < 9 then  -- rising edge sync_spi_clk_in 
-						rx_buffer <= (rx_buffer(8 downto bit_index+1) & sync_spi_data_in(2) & rx_buffer(bit_index-1 downto 0));
-						bit_index <= bit_index + 1;
-
-						data_received_out <= '0';
-						data_valid_out <= '1';
-
-					elsif bit_index >= 9 then
+					if sync_spi_clk_in(2) = '0' and sync_spi_clk_in(1) = '1' and n_received_bits <= 8 then  -- rising edge sync_spi_clk_in 
 						
+						rx_buffer(8 downto 1) <= rx_buffer(7 downto 0);
+						rx_buffer(0) <= sync_spi_data_in(1);
+
+						n_received_bits <= n_received_bits + 1;
+					end if;
+
+					if n_received_bits = 9 then
 						-- calculate even parity bit
-						v_parity_target := xor rx_buffer; 
-						
-						-- check if message has parity error
+						v_parity_target := xor rx_buffer(8 downto 1); -- exclude real parity bit
+							
+						-- check if message has parity error. sync_spi_data_in(1) is received parity bit
 						if rx_buffer(0) /= v_parity_target then
 							data_valid_out <= '0';
 						else 
 							data_valid_out <= '1';
 						end if;
-						
+							
 						data_rx_out <= rx_buffer(8 downto 1);
 						data_received_out <= '1';
-						
+					else
+						data_valid_out <= '1';
+						data_received_out <= '0';
+						data_rx_out <= (others => '0');
 					end if;
+					
 
 				when others =>
 					next_state := IDLE;
